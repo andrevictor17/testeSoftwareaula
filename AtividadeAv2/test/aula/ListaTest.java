38d247a242c1a644dd46810efe9c353162a79889
andrevictor17 <andre.victor.1996@gmail.com>
2017-04-04 09:05:33 -0300

    Update ListaTest.java

--- AtividadeAv2/test/aula/ListaTest.java
+++ AtividadeAv2/test/aula/ListaTest.java
@@ -12,37 +12,37 @@ public class ListaTest {
 	@Before
 	public void setUp()
 	{
-		when(list.get(0)).thenReturn("Mauricio");
-		when(list.get(1)).thenReturn("Thiago");
-		when(list.get(2)).thenReturn("João Pedro");
+		when(list.get(0)).thenReturn("Andre");
+		when(list.get(1)).thenReturn("Gabriel");
+		when(list.get(2)).thenReturn("Jullyana");
 		when(list.get(3)).thenThrow(ArrayIndexOutOfBoundsException.class);
 	}
 	
 	@Test(expected=ArrayIndexOutOfBoundsException.class)
 	public void test1() {
-		list.add("Mauricio");
-		list.add("Thiago");
-		list.add("João Pedro");
+		list.add("Andre");
+		list.add("Gabriel");
+		list.add("Jullyana");
 		list.get(3);
 	}
 	@Test
 	public void test2() {
-		list.add("Mauricio");
-		assertTrue(list.get(0)=="Mauricio");
+		list.add("Andre");
+		assertTrue(list.get(0)=="Andre");
 		
 		
 	}
 	public void test3() {
-		list.add("Mauricio");
-		list.add("Thiago");
-		assertTrue(list.get(1)=="Thiago");
+		list.add("Andre");
+		list.add("Gabriel");
+		assertTrue(list.get(1)=="Gabriel");
 		
 	}
 	public void test4() {
-		list.add("Mauricio");
-		list.add("Thiago");
-		list.add("João Pedro");
-		assertTrue(list.get(2)=="João Pedro");
+		list.add("Andre");
+		list.add("Gabriel");
+		list.add("Jullyana");
+		assertTrue(list.get(2)=="Jullyana");
 		
 	}
 
